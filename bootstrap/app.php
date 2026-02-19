0a87c8ec5c56cbdc8294c1b5f7c59bcbdbeb0f47
Gui <guillaume.leguludec@gmail.com>
2026-02-19 22:13:11 +0100

    feat: enhance exception handling for user authentication
    - Added renderable exceptions for InvalidPasswordException and UserAlreadyExistException in the application bootstrap file.
    - Improved error handling by throwing validation exceptions with specific messages for password and email issues during user authentication.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,13 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
-use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use App\Features\Auth\Domain\Exceptions\InvalidPasswordException;
+use App\Features\Auth\Domain\Exceptions\UserAlreadyExistException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -23,5 +25,15 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (InvalidPasswordException $e) {
+            throw \Illuminate\Validation\ValidationException::withMessages([
+                'password' => $e->getMessage(),
+            ]);
+        });
+    
+        $exceptions->renderable(function (UserAlreadyExistException $e) {
+            throw \Illuminate\Validation\ValidationException::withMessages([
+                'email' => $e->getMessage(),
+            ]);
+        });
     })->create();
