4ac7ec9fd388380c138b1e7b7ed71128d8cf4975
Gui <guillaume.leguludec@gmail.com>
2026-03-07 12:35:47 +0100

    test: Add test for email already exist and invalid password for register user use case

--- tests/Unit/Features/Auth/Application/Command/RegisterUser/RegisterUserCommandHandlerTest.php
+++ tests/Unit/Features/Auth/Application/Command/RegisterUser/RegisterUserCommandHandlerTest.php
@@ -8,11 +8,14 @@ use App\Features\Auth\Application\Commands\RegisterUser\RegisterUserCommand;
 use App\Features\Auth\Application\Commands\RegisterUser\RegisterUserCommandHandler;
 use App\Features\Auth\Domain\Entities\User;
 use App\Features\Auth\Domain\Events\UserCreated;
+use App\Features\Auth\Domain\Exceptions\InvalidPasswordException;
+use App\Features\Auth\Domain\Exceptions\UserAlreadyExistException;
 use Tests\Shared\Mother\Fake\FakePasswordHasher;
 use Tests\Shared\Mother\Fake\FakeUuidGenerator;
 use Tests\Shared\Mother\Fake\ImmediateTransactionManager;
 use Tests\Shared\Mother\Fake\InMemoryUserRepository;
 use Tests\Shared\Mother\Fake\SpyEventDispatcher;
+use Tests\Shared\Mother\UserMother;
 use Tests\TestCase;
 
 /**
@@ -22,20 +25,30 @@ use Tests\TestCase;
  */
 final class RegisterUserCommandHandlerTest extends TestCase
 {
+    private InMemoryUserRepository $repository;
+    private ImmediateTransactionManager $tx;
+    private FakeUuidGenerator $uuidGenerator;
+    private FakePasswordHasher $passwordHasher;
+    private SpyEventDispatcher $eventDispatcher;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repository = new InMemoryUserRepository();
+        $this->tx = new ImmediateTransactionManager();
+        $this->uuidGenerator = new FakeUuidGenerator();
+        $this->passwordHasher = new FakePasswordHasher();
+        $this->eventDispatcher = new SpyEventDispatcher();
+    }
     public function testItRegistersAUser(): void
     {
-        $repository = new InMemoryUserRepository();
-        $tx = new ImmediateTransactionManager();
-        $uuidGenerator = new FakeUuidGenerator();
-        $passwordHasher = new FakePasswordHasher();
-        $eventDispatcher = new SpyEventDispatcher();
-
         $handler = new RegisterUserCommandHandler(
-            $repository,
-            $tx,
-            $passwordHasher,
-            $uuidGenerator,
-            $eventDispatcher
+            $this->repository,
+            $this->tx,
+            $this->passwordHasher,
+            $this->uuidGenerator,
+            $this->eventDispatcher
         );
 
         $command = new RegisterUserCommand(
@@ -46,11 +59,56 @@ final class RegisterUserCommandHandlerTest extends TestCase
 
         $result = $handler($command);
 
-        $user = $repository->findById($result->value()->id());
+        $user = $this->repository->findById($result->value()->id());
 
         $this->assertNotNull($user);
         $this->assertTrue($result->isOk());
         $this->assertInstanceOf(User::class, $result->value());
-        $this->assertTrue($eventDispatcher->dispatched(UserCreated::class));
+        $this->assertTrue($this->eventDispatcher->dispatched(UserCreated::class));
+    }
+
+    public function testItReturnsAnExceptionIfTheUserAlreadyExists(): void
+    {
+        $this->repository->create(UserMother::register());
+        $handler = new RegisterUserCommandHandler(
+            $this->repository,
+            $this->tx,
+            $this->passwordHasher,
+            $this->uuidGenerator,
+            $this->eventDispatcher
+        );
+
+        $command = new RegisterUserCommand(
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+            password: 'Password123!',
+        );
+
+        $result = $handler($command);
+        $this->assertTrue($result->isFail());
+        $this->assertInstanceOf(UserAlreadyExistException::class, $result->error());
+        $this->assertStringContainsString('USER_ALREADY_EXISTS', $result->error()->getDomainCode());
+    }
+
+    public function testItReturnsAnExceptionIfThePasswordIsInvalid(): void
+    {
+        $handler = new RegisterUserCommandHandler(
+            $this->repository,
+            $this->tx,
+            $this->passwordHasher,
+            $this->uuidGenerator,
+            $this->eventDispatcher
+        );
+
+        $command = new RegisterUserCommand(
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+            password: 'invalid',
+        );
+
+        $result = $handler($command);
+        $this->assertTrue($result->isFail());
+        $this->assertInstanceOf(InvalidPasswordException::class, $result->error());
+        $this->assertStringContainsString('INVALID_PASSWORD', $result->error()->getDomainCode());
     }
 }
