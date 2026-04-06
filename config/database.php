a5185058a3cac6fdfc1770f07af43904ea4ad973
Gui <guillaume.leguludec@gmail.com>
2026-04-06 16:15:54 +0200

    refactor: remove PLAYER_PROFILES_CONNECTION and related configurations
    - Deleted PLAYER_PROFILES_CONNECTION from .env.example and .env.testing files to streamline database connection settings.
    - Removed the connection logic from the Player model, simplifying its structure.
    - Updated database.php to adjust the search_path for PostgreSQL connections.
    - Deleted the player.php configuration file as it was no longer needed.

--- config/database.php
+++ config/database.php
@@ -93,22 +93,7 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'private',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
-        ],
-
-        'pgsql_public' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
+            'search_path' => ['private', 'public'],
             'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
